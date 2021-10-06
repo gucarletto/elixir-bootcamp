d0c12237016f73bc4827073aa05a1243424e2fbb
Gustavo Carletto <gucarletto@gmail.com>
2021-10-06 16:34:48 -0300

    Class 3

--- lib/wabanex/imc.ex
+++ lib/wabanex/imc.ex
@@ -11,6 +11,7 @@ defmodule Wabanex.IMC do
       |> String.split("\n")
       |> Enum.map(fn line -> parse_line(line) end)
       |> Enum.into(%{})
+
     {:ok, data}
   end
 
--- lib/wabanex/users/create.ex
+++ lib/wabanex/users/create.ex
@@ -1,11 +1,9 @@
 defmodule Wabanex.Users.Create do
+  alias Wabanex.{Repo, User}
 
-    alias Wabanex.{Repo, User}
-
-    def call(params) do
-      params
-      |> User.changeset()
-      |> Repo.insert()
-    end
-
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+  end
 end
